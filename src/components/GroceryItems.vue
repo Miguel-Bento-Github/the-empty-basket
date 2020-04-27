432523c7a9f8542ba2ff1405739486cb3a694ffd
Miguel Bento <miguel.bento@eurail.com>
2020-04-27 17:51:51 +0200

    BUG - Dark theme item-details color

--- src/components/GroceryItems.vue
+++ src/components/GroceryItems.vue
@@ -15,7 +15,7 @@
               i % Math.floor(Math.random() * 2) === 0 && {
                 filter: `drop-shadow(0 0 ${Math.floor(Math.random() * 8)}px ${
                   colors[i]
-                })`,
+                })`
               }
             "
             v-for="(letter, i) in title"
@@ -75,123 +75,124 @@
 </template>
 
 <script>
-  export default {
-    name: 'GroceryItem',
-    props: ['products', 'colors', 'isTyping', 'loading', 'showHeader', 'theme'],
-    data() {
-      return {
-        name: '',
-        price: 0,
-        unit: '',
-        category: '',
-        headers: ['cat.', 'name', 'price', 'unit'],
-        showDetails: {
-          active: false,
-          index: 0,
-        },
-      };
+export default {
+  name: "GroceryItem",
+  props: ["products", "colors", "isTyping", "loading", "showHeader", "theme"],
+  data() {
+    return {
+      name: "",
+      price: 0,
+      unit: "",
+      category: "",
+      headers: ["cat.", "name", "price", "unit"],
+      showDetails: {
+        active: false,
+        index: 0
+      }
+    };
+  },
+  methods: {
+    detectMob() {
+      return window.innerWidth <= 450 && window.innerWidth <= 600;
     },
-    methods: {
-      detectMob() {
-        return window.innerWidth <= 450 && window.innerWidth <= 600;
-      },
-      openDetails(index) {
-        this.showDetails.active = Boolean(index);
-        this.showDetails.index = index;
-      },
-    },
-    computed: {
-      titles() {
-        const headers = [...this.headers];
-        if (!this.detectMob()) {
-          headers[0] = 'category';
-        }
-        return headers;
-      },
-    },
-  };
+    openDetails(index) {
+      this.showDetails.active = Boolean(index);
+      this.showDetails.index = index;
+    }
+  },
+  computed: {
+    titles() {
+      const headers = [...this.headers];
+      if (!this.detectMob()) {
+        headers[0] = "category";
+      }
+      return headers;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .table {
-    margin-top: 5vh;
+.table {
+  margin-top: 5vh;
 
-    &-header,
-    &-body {
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
-      text-transform: capitalize;
-      cursor: default;
-    }
+  &-header,
+  &-body {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+    text-transform: capitalize;
+    cursor: default;
+  }
 
-    &-body {
-      transition: color 150ms linear, background 150ms linear;
+  &-body {
+    transition: color 150ms linear, background 150ms linear;
 
-      &.dark {
-        &:nth-child(even) {
-          background: rgba(#f4f4f4, 0.2);
-        }
-
-        &:hover {
-          color: #1a2530;
-          background: rgba(#f4f4f4, 0.6);
-        }
+    &.dark {
+      &:nth-child(even) {
+        background: rgba(#f4f4f4, 0.2);
       }
 
-      &.light {
-        &:nth-child(odd) {
-          background: rgba(#1a2530, 0.2);
-        }
-
-        &:hover {
-          color: #f4f4f4;
-          background: rgba(#1a2530, 0.6);
-        }
+      &:hover {
+        color: #1a2530;
+        background: rgba(#f4f4f4, 0.6);
       }
     }
 
-    &-item {
-      width: 100%;
-      padding: 5px;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      overflow: hidden;
+    &.light {
+      &:nth-child(odd) {
+        background: rgba(#1a2530, 0.2);
+      }
 
-      &-details {
-        transition: all 250ms 50ms linear;
-
-        &:hover {
-          width: max-content;
-          position: absolute;
-          background: #1a2530;
-          padding: 0 8px;
-        }
+      &:hover {
+        color: #f4f4f4;
+        background: rgba(#1a2530, 0.6);
       }
     }
+  }
 
-    .control {
-      display: flex;
-      align-items: center;
+  &-item {
+    width: 100%;
+    padding: 5px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+
+    &-details {
+      transition: all 250ms 50ms linear;
+
+      &:hover {
+        width: max-content;
+        position: absolute;
+        background: #1a2530;
+        color: #f4f4f4;
+        padding: 0 8px;
+      }
     }
   }
 
-  .letter {
-    transition: color 500ms ease-out;
+  .control {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.letter {
+  transition: color 500ms ease-out;
+}
+
+.fade {
+  &-enter-active {
+    transition: all 0.3s ease-out;
   }
 
-  .fade {
-    &-enter-active {
-      transition: all 0.3s ease-out;
-    }
-
-    &-leave-active {
-      transition: all 0.5s ease-out;
-    }
-
-    &-enter,
-    &-leave-to {
-      transform: translateY(-10px);
-      opacity: 0;
-    }
+  &-leave-active {
+    transition: all 0.5s ease-out;
   }
+
+  &-enter,
+  &-leave-to {
+    transform: translateY(-10px);
+    opacity: 0;
+  }
+}
 </style>
