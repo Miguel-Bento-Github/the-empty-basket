3721d2a3cb28b904980d85bc42d722a480affa7e
Miguel Bento <miguel.bento@eurail.com>
2020-04-28 21:17:52 +0200

    Add style function helper to letters
    - This patch improves semantic and adds a helper.

--- src/components/GroceryItems.vue
+++ src/components/GroceryItems.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="table">
+  <section class="table">
     <transition name="fade">
-      <div v-show="showHeader" class="table-header">
+      <header v-show="showHeader" class="table-header">
         <h1 class="title" v-for="title in titles" :key="title.id">
           <!--
           Style bind starts by finding all letters
@@ -11,17 +11,13 @@
           <span
             class="letter"
             :class="isTyping && 'letter-active'"
-            :style="
-              i % getRandomIntFrom(2) === 0 && {
-                filter: `drop-shadow(0 0 ${getRandomIntFrom(8)}px ${colors[i]})`
-              }
-            "
+            :style="letterStyle(i)"
             v-for="(letter, i) in title"
             :key="letter.id"
             >{{ letter }}</span
           >
         </h1>
-      </div>
+      </header>
     </transition>
     <transition-group name="fade">
       <div
@@ -63,132 +59,139 @@
         <div class="table-item" name="unit">{{ product.unit }}</div>
       </div>
     </transition-group>
-  </div>
+  </section>
 </template>
 
 <script>
-import getRandomIntFrom from "../utils/getRandomIntFrom";
-export default {
-  name: "GroceryItem",
-  props: ["products", "colors", "isTyping", "loading", "showHeader", "theme"],
-  data() {
-    return {
-      name: "",
-      price: 0,
-      unit: "",
-      category: "",
-      headers: ["cat.", "name", "price", "unit"],
-      showDetails: {
-        active: false,
-        index: 0
-      }
-    };
-  },
-  methods: {
-    getRandomIntFrom,
-
-    detectMob() {
-      return window.innerWidth <= 450 && window.innerWidth <= 600;
+  import getRandomIntFrom from '../utils/getRandomIntFrom';
+  export default {
+    name: 'GroceryItem',
+    props: ['products', 'colors', 'isTyping', 'loading', 'showHeader', 'theme'],
+    data() {
+      return {
+        name: '',
+        price: 0,
+        unit: '',
+        category: '',
+        headers: ['cat.', 'name', 'price', 'unit'],
+        showDetails: {
+          active: false,
+          index: 0,
+        },
+      };
     },
+    methods: {
+      letterStyle(i) {
+        return (
+          i % getRandomIntFrom(2) === 0 && {
+            filter: `drop-shadow(0 0 ${getRandomIntFrom(8)}px ${
+              this.colors[i]
+            })`,
+          }
+        );
+      },
+      detectMob() {
+        return window.innerWidth <= 450 && window.innerWidth <= 600;
+      },
 
-    openDetails(index) {
-      this.showDetails.active = Boolean(index);
-      this.showDetails.index = index;
-    }
-  },
-  computed: {
-    titles() {
-      const headers = [...this.headers];
-      if (!this.detectMob()) {
-        headers[0] = "category";
-      }
-      return headers;
-    }
-  }
-};
+      openDetails(index) {
+        this.showDetails.active = Boolean(index);
+        this.showDetails.index = index;
+      },
+    },
+    computed: {
+      titles() {
+        const headers = [...this.headers];
+        if (!this.detectMob()) {
+          headers[0] = 'category';
+        }
+        return headers;
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.table {
-  margin-top: 5vh;
+  .table {
+    margin-top: 5vh;
 
-  &-header,
-  &-body {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
-    text-transform: capitalize;
-    cursor: default;
-  }
+    &-header,
+    &-body {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+      text-transform: capitalize;
+      cursor: default;
+    }
 
-  &-body {
-    transition: color 150ms linear, background-color 150ms linear;
+    &-body {
+      transition: color 150ms linear, background-color 150ms linear;
 
-    &.dark {
-      &:nth-child(even) {
-        background-color: rgba(#f4f4f4, 0.2);
+      &.dark {
+        &:nth-child(even) {
+          background-color: rgba(#f4f4f4, 0.2);
+        }
+
+        &:hover {
+          color: #1a2530;
+          background-color: rgba(#f4f4f4, 0.6);
+        }
       }
 
-      &:hover {
-        color: #1a2530;
-        background-color: rgba(#f4f4f4, 0.6);
+      &.light {
+        &:nth-child(odd) {
+          background-color: rgba(#1a2530, 0.2);
+        }
+
+        &:hover {
+          color: #f4f4f4;
+          background-color: rgba(#1a2530, 0.6);
+        }
       }
     }
 
-    &.light {
-      &:nth-child(odd) {
-        background-color: rgba(#1a2530, 0.2);
-      }
+    &-item {
+      width: 100%;
+      padding: 5px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
 
-      &:hover {
-        color: #f4f4f4;
-        background-color: rgba(#1a2530, 0.6);
+      &-details {
+        transition: all 250ms 50ms linear;
+
+        &:hover {
+          width: max-content;
+          position: absolute;
+          background: #1a2530;
+          color: #f4f4f4;
+          padding: 0 8px;
+        }
       }
+    }
+
+    .control {
+      display: flex;
+      align-items: center;
     }
   }
 
-  &-item {
-    width: 100%;
-    padding: 5px;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
+  .letter {
+    transition: color 500ms ease-out;
+  }
 
-    &-details {
-      transition: all 250ms 50ms linear;
+  .fade {
+    &-enter-active {
+      transition: all 0.3s ease-out;
+    }
 
-      &:hover {
-        width: max-content;
-        position: absolute;
-        background: #1a2530;
-        color: #f4f4f4;
-        padding: 0 8px;
-      }
+    &-leave-active {
+      transition: all 0.5s ease-out;
+    }
+
+    &-enter,
+    &-leave-to {
+      transform: translateY(-10px);
+      opacity: 0;
     }
   }
-
-  .control {
-    display: flex;
-    align-items: center;
-  }
-}
-
-.letter {
-  transition: color 500ms ease-out;
-}
-
-.fade {
-  &-enter-active {
-    transition: all 0.3s ease-out;
-  }
-
-  &-leave-active {
-    transition: all 0.5s ease-out;
-  }
-
-  &-enter,
-  &-leave-to {
-    transform: translateY(-10px);
-    opacity: 0;
-  }
-}
 </style>
